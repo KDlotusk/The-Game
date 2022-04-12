a9e42be4bcc7148abf4fa715ff29280e77ec711d
theo labrosse <theo.labrosse@etu.univ-lyon1.fr>
2022-04-12 14:48:38 +0200

    updating Hand.hpp and Stack.hpp
    Correcting a warning about a non existant destructor

--- src/server/models/cards/Hand.hpp
+++ src/server/models/cards/Hand.hpp
@@ -9,6 +9,7 @@ namespace theGame {
     class Hand : public Stack {
     public:
         Hand(const std::vector<Card>& cards);
+        ~Hand() override {};
 
         Card getCard(const int& i) const;
         void addCard(const Card& card);
